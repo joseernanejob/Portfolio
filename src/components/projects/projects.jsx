3ff2a6330bf334209d51acaf159b07d7014b4e25
joseernanejob <jose.ernane.job@gmail.com>
2022-12-30 11:59:21 -0300

    Portfolio

---
+++ src/components/projects/projects.jsx
@@ -0,0 +1,28 @@
+import './projects.css';
+
+import LoginTravel from '../../assets/Logintravel.png';
+import RobotLogin from '../../assets/RobotLogin.png';
+
+export default function Projects() {
+  return (
+    <div className='container-projects' id='projects'>
+      <h2 className='title-projects'>Projects</h2>
+      <div className='images-projects'>
+        <div className='item-projects'>
+          <a
+            href={'https://github.com/joseernanejob/LoginTravel.git'}
+            target={'_blank'}
+          >
+            <img src={LoginTravel} alt='' />
+          </a>
+          <a
+            href={'https://github.com/joseernanejob/Robot-Login.git'}
+            target={'_blank'}
+          >
+            <img src={RobotLogin} alt='' />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}

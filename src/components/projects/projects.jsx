d52a8f5997767747e84bf97cb1d86565869fdce8
joseernanejob <jose.ernane.job@gmail.com>
2022-12-30 12:05:49 -0300

    Portfolio

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,6 +1,5 @@
 import './projects.css';
 
-import LoginTravel from '../../assets/Logintravel.png';
 import RobotLogin from '../../assets/RobotLogin.png';
 
 export default function Projects() {
@@ -9,12 +8,6 @@ export default function Projects() {
       <h2 className='title-projects'>Projects</h2>
       <div className='images-projects'>
         <div className='item-projects'>
-          <a
-            href={'https://github.com/joseernanejob/LoginTravel.git'}
-            target={'_blank'}
-          >
-            <img src={LoginTravel} alt='' />
-          </a>
           <a
             href={'https://github.com/joseernanejob/Robot-Login.git'}
             target={'_blank'}
